5e473dcada7444bc535b8581004892b470d15c38
Prajun Trital <prajuncs@gmail.com>
2022-03-23 20:36:23 -0500

    user-entry

--- client/src/components/pages/userEntry/UserEntry.js
+++ client/src/components/pages/userEntry/UserEntry.js
@@ -1,11 +1,11 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext} from 'react';
 import { UserContext } from '../../../contexts/UserContext';
 
 const API_BASE = "http://localhost:5000";
 
 function UserEntry() {
 
-    const {loggedIn, user } = useContext(UserContext);
+    const {user } = useContext(UserContext);
 
     const[userName, setUserName] = useState("");
     const[userProfession, setUserProfession] = useState("");
@@ -23,7 +23,6 @@ function UserEntry() {
     const[userPolitics, setUserPolitics] = useState("");
 
    
-
     const addContact = async () => {
         await fetch(
           `${API_BASE}/contact`,{
@@ -32,24 +31,36 @@ function UserEntry() {
               "Content-Type" : "application/json"
             },
             body : JSON.stringify({
-            "user_id" : `${user._id}`,
-             "name" : userName, 
-             "profession" : userProfession,
-             "location" : userLocation,
-             "business" : userBusiness,
-             "finance" : userFinance,
-             "institution": userInstitution,
-             "skillset": userSkillset,
-             "mobile": userMobile,
-             "dateOfBirth": userDateOfBirth,
-             "linkedIn": userLinkedIn,
-             "bloodGroup": userBloodGroup,
-             "facebook": userFacebook
+                  "user_id" : `${user._id}`,
+                  "name" : userName, 
+                  "profession" : userProfession,
+                  "location" : userLocation,
+                  "business" : userBusiness,
+                  "finance" : userFinance,
+                  "institution": userInstitution,
+                  "skillset": userSkillset,
+                  "mobile": userMobile,
+                  "dateOfBirth": userDateOfBirth,
+                  "linkedIn": userLinkedIn,
+                  "bloodGroup": userBloodGroup,
+                  "facebook": userFacebook
             })
           }
         );
         setUserName("");
         setUserProfession("");
+        setUserLocation("");
+        setUserBusiness("");
+        setUserFinance("");
+        setUserInstitution("");
+        setUserSkillset("");
+        setUserMobile("");
+        setUserDateOfBirth("");
+        setUserLinkedIn("");
+        setUserBloodGroup("");
+        setUserFacebook("");
+        setUserCanHelpMeAt("");
+        setUserPolitics("");
       }
 
   return (
@@ -60,89 +71,92 @@ function UserEntry() {
               <label>Name </label>
               <input 
                     type ="text" 
-                    className = "add-menu-text" 
                     onChange = {e => setUserName(e.target.value)}
                     value = {userName}/>
 
               <label>Profession</label>  
               <input 
                     type ="text" 
-                    className = "add-userLocation-text" 
                     onChange = {e => setUserLocation(e.target.value)}
                     value = {userLocation}/>
 
               <label>User Location</label>
               <input 
                     type ="text" 
-                    className = "add-profession-text" 
                     onChange = {e => setUserProfession(e.target.value)}
                     value = {userProfession}/>
 
               <label>Business</label>
               <input 
                     type ="text" 
-                    className = "add-business-text" 
                     onChange = {e => setUserBusiness(e.target.value)}
                     value = {userBusiness}/>
 
               <label>Finance</label>
               <input 
                     type ="text" 
-                    className = "add-finance-text" 
                     onChange = {e => setUserFinance(e.target.value)}
                     value = {userFinance}/>
 
               <label>Institution</label>
               <input 
                     type ="text" 
-                    className = "add-Institution-text" 
                     onChange = {e => setUserInstitution(e.target.value)}
                     value = {userInstitution}/>
 
               <label>Skillset</label>
               <input 
                     type ="text" 
-                    className = "add-skillset-text" 
                     onChange = {e => setUserSkillset(e.target.value)}
                     value = {userSkillset}/>
 
               <label>Mobile</label>
               <input 
-                    type ="text" 
-                    className = "add-mobile-text" 
+                    type ="tel" 
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    placeholder="999-999-9999"
                     onChange = {e => setUserMobile(e.target.value)}
                     value = {userMobile}/>
 
               <label>Date of Birth</label>
               <input 
-                    type ="text" 
-                    className = "add-birthdate-text" 
+                    type ="date" 
                     onChange = {e => setUserDateOfBirth(e.target.value)}
                     value = {userDateOfBirth}/>
 
               <label>LinkedIn</label>
               <input 
                     type ="text" 
-                    className = "add-linkedIn-text" 
                     onChange = {e => setUserLinkedIn(e.target.value)}
                     value = {userLinkedIn}/>
 
               <label>Blood Group</label>
               <input 
                     type ="text" 
-                    className = "add-bloodGroup-text" 
                     onChange = {e => setUserBloodGroup(e.target.value)}
                     value = {userBloodGroup}/>
 
               <label>Facebook</label>
               <input 
                     type ="text" 
-                    className = "add-facebook-text" 
                     onChange = {e => setUserFacebook(e.target.value)}
                     value = {userFacebook}/>
 
+              <label>Can Help Me At</label>
+              <input 
+                    type ="text" 
+                    onChange = {e => setUserCanHelpMeAt(e.target.value)}
+                    value = {userCanHelpMeAt}/>
+
+
+              <label>Politics</label>
+              <input 
+                    type ="text" 
+                    onChange = {e => setUserPolitics(e.target.value)}
+                    value = {userPolitics}/>
+
               
-              <div className='add-menu-button' onClick = {addContact}> Save </div>
+              <button onClick = {addContact}> Save </button>
            </div>
 
 
