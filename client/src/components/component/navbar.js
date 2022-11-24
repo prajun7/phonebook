6536d6ce245ef150276a93bebfbc922211664cd5
Ayusha Khadka <akhadkacs@gmail.com>
2022-11-23 19:50:17 -0600

    added navbar

---
+++ client/src/components/component/navbar.js
@@ -0,0 +1,185 @@
+import React, { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import logoCircle from "../../images/logoCircle.png";
+
+function ResponsiveAppBar() {
+  const history = useHistory();
+  const { loggedIn, setLoggedIn, setUser } = useContext(UserContext);
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleLogin = (event) => {
+    setAnchorElNav(event.currentTarget);
+    history.push("/login");
+  };
+
+  const handleSignup = (event) => {
+    setAnchorElNav(event.currentTarget);
+    history.push("/signup");
+  };
+
+  const handleDashboard = (event) => {
+    setAnchorElNav(event.currentTarget);
+    history.push("/dashboard");
+  };
+
+  const handleLogout = (event) => {
+    setAnchorElNav(event.currentTarget);
+    setLoggedIn(false);
+    setUser(null);
+    history.push("/");
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PHONE-BOOK
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleLogin}>
+                <Typography textAlign="center">logIn</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleSignup}>
+                <Typography textAlign="center">Signup</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PHONE BOOK
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <MenuItem onClick={handleLogin}>
+              <Typography textAlign="center">logIn</Typography>
+            </MenuItem>
+            <MenuItem onClick={handleSignup}>
+              <Typography textAlign="center">Signup</Typography>
+            </MenuItem>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="PhoneBook" src={logoCircle} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleDashboard}>
+                <Typography textAlign="center">Dashboard</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,65 +1,49 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../../../contexts/UserContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../../contexts/UserContext";
 
 // This will be the Home Page,
 // User will be able to SigUp and LogIn from this page, if they don't have an account
 
 function Home() {
-
-    // Getting user info from the context
-    const {loggedIn, setLoggedIn, setUser} = useContext(UserContext);
+  // Getting user info from the context
+  const { loggedIn, setLoggedIn, setUser } = useContext(UserContext);
 
   // Calling this function, to logout the user
   // To logout the user we are setting LoggedIn to false and clearing the User data to null
-  function LoggOutButton(){
+  function LoggOutButton() {
     setLoggedIn(false);
     setUser(null);
-}
+  }
 
   return (
-  <div>
-     <h1> 
-       <Link to='/' >
-            PHONEBOOK
-        </Link> 
+    <div>
+      <h1>
+        <Link to="/">PHONEBOOK</Link>
       </h1>
 
-      
-        {loggedIn?
-      //True
+      {loggedIn ? (
+        //True
         <ul>
-           <li>
-           <Link
-                    to='/'
-                    className='nav-links'
-                    onClick={LoggOutButton}
-                  >
-                    LogOut
-                    </Link>
-              </li>
-          </ul>
-
-        :
-        
+          <li>
+            <Link to="/" className="nav-links" onClick={LoggOutButton}>
+              LogOut
+            </Link>
+          </li>
+        </ul>
+      ) : (
         //False
-      <ul>
-        <li>
-          <Link
-              to='/login'>
-                    LogIn
-          </Link>
-        </li>
+        <ul>
+          <li>
+            <Link to="/login">LogIn</Link>
+          </li>
 
-        <li>
-          <Link
-              to='/signup'>
-                    SignUp
-          </Link>
-        </li>
-      </ul>
-      }
-  </div>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 }
 
